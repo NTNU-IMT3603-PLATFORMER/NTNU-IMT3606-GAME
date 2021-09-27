41c88d6b392b286bf5c3e0b3cd25aa5de7f25e79
Ruben Christoffer <rubenc@protonmail.com>
2021-09-27 22:07:37 +0200

    Scripts: CharacterController2D: Added isFacingRight property. Refactored

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -6,24 +6,50 @@ public class CharacterController2D : MonoBehaviour {
 
     public SpriteRenderer spriteRenderer;
     public Rigidbody2D rb;
+
     public Transform groundCheckPoint;
-    public float groundRadius = 0.41f;
-    public UnityEngine.LayerMask groundMask;
+    public float groundCheckRadius = 0.41f;
+    public UnityEngine.LayerMask groundCheckMask;
+
+    public Transform wallCheckPoint;
+    public float wallCheckRadius = 0.41f;
+    public UnityEngine.LayerMask wallCheckMask;
+
     public float jumpHeight = 5f;
     public float jumpResetTime = 0.5f;
     public bool flipIfChangingDirection = true;
+    public bool startFacingRight = true;
 
-    public bool grounded { get; private set; }
+    public bool isGrounded { get; private set; }
+    public bool isFacingWall { get; private set; }
+    public bool isFacingRight { get; private set; }
 
     private float jumpTime;
 
-    public void Move (Vector2 movement, bool jump) {
-        Vector2 targetVelocity = rb.velocity;
-        targetVelocity.x = movement.x;
-        targetVelocity.y += movement.y;
+    void Start () {
+        isFacingRight = startFacingRight;
+    }
 
+    public void Move (Vector2 movement, bool jump) {
+        // Create a target velocity that will be applied later in the script
+        // Include gravity for y component
+        Vector2 targetVelocity = new Vector2(movement.x, rb.velocity.y + movement.y);
+
+        // Update properties relating to state of player
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundCheckMask) != null;
+        isFacingWall = Physics2D.OverlapCircle(wallCheckPoint.position, wallCheckRadius, wallCheckMask) != null;
+        
+        if (isFacingRight && movement.x < 0f) {
+            isFacingRight = false;
+        }
+
+        if (!isFacingRight && movement.x > 0f) {
+            isFacingRight = true;
+        }
+
+        // Jump-related logic
         if (jumpTime <= 0f) {
-            if (jump && grounded) {
+            if (jump && isGrounded) {
                 // Jump (calculate target velocity based on jump height and gravity)
                 targetVelocity.y = Mathf.Sqrt(2 * -Physics2D.gravity.y * rb.gravityScale * jumpHeight);
 
@@ -33,23 +59,11 @@ public class CharacterController2D : MonoBehaviour {
             jumpTime -= Time.fixedDeltaTime;
         }
 
-        if (flipIfChangingDirection) {
-            // Handle flipping of character when moving direction
-
-            if (!spriteRenderer.flipX && movement.x < 0f) {
-                spriteRenderer.flipX = true;
-            }
-
-            if (spriteRenderer.flipX && movement.x > 0f) {
-                spriteRenderer.flipX = false;
-            }
-        }
-
         // Apply target velocity
         rb.velocity = targetVelocity;
 
-        // Check if grounded
-        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundRadius, groundMask) != null;
+        // Flip sprite renderer when facing another direction
+        spriteRenderer.flipX = startFacingRight ? !isFacingRight : isFacingRight;
     }
 
 }
