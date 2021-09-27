c4be3ba5c6a9d40d44e030fce09bd4550437c977
Ruben Christoffer <rubenc@protonmail.com>
2021-09-27 20:09:31 +0200

    Scripts: PlayerMovement: Converted to using dynamic rigidbody

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -5,89 +5,42 @@ using UnityEngine;
 public class CharacterController2D : MonoBehaviour {
 
     public Rigidbody2D rb;
-    public Transform groundRayOrigin;
-    public float groundRayLength = 0.1f;
+    public Transform groundCheckPoint;
+    public float groundRadius = 0.41f;
     public UnityEngine.LayerMask groundMask;
-    public bool applyGravity = true;
-    public float gravityFactor = 1f;
-    public float jumpFactor = 0.5f;
-    public bool flip = true;
+    public float jumpHeight = 5f;
+    public float jumpResetTime = 0.5f;
+    public bool flipIfChangingDirection = true;
 
     public bool grounded { get; private set; }
-    public RaycastHit2D lastGroundHit { get; private set; }
-
-    // Forces when mass is 1 (constant)
-    private Dictionary<string, Vector2> forces;
 
     private float jumpTime;
-    private Vector2 currentForceAppliedVelocity;
-
-    void Awake () {
-        forces = new Dictionary<string, Vector2>() {
-            {"g", Physics2D.gravity * gravityFactor},
-            {"n", -Physics2D.gravity * gravityFactor}
-         };
-    }
 
     public void Move (Vector2 movement, bool jump) {
-        // Set normal force when grounded
-        if (grounded) {
-            forces["n"] = -forces["g"];
-        } else {
-            forces["n"] = Vector3.zero;
-        }
-
-        // Add up normalized forces for resulting acceleration
-        Vector2 resultingAcceleration = Vector2.zero;
-
-        foreach (Vector2 force in forces.Values) {
-            resultingAcceleration += force;
-        }
+        Vector2 targetVelocity = rb.velocity;
+        targetVelocity.x = movement.x;
+        targetVelocity.y += movement.y;
 
         if (jumpTime <= 0f) {
             if (jump && grounded) {
-                jumpTime = 0.5f;
-                currentForceAppliedVelocity = -forces["g"] * jumpFactor;
-            }
+                // Jump
+                targetVelocity.y = Mathf.Sqrt(2 * -Physics2D.gravity.y * rb.gravityScale * jumpHeight);
 
-            if (!jump && grounded) {
-                // Reset velocity when grounded
-                currentForceAppliedVelocity = Vector2.zero;
+                jumpTime = jumpResetTime;
             }
         } else {
-            jumpTime -= Time.deltaTime;
+            jumpTime -= Time.fixedDeltaTime;
         }
 
-        // Adjust velocity based on calculated resulting acceleration
-        currentForceAppliedVelocity += resultingAcceleration * Time.deltaTime;
-        movement += currentForceAppliedVelocity;
-
-        if (flip) {
+        if (flipIfChangingDirection) {
             transform.localScale = new Vector3(movement.x < 0f ? -1f : 1f, transform.localScale.y, transform.localScale.z);
         }
 
-        // Apply movement
-        transform.position += movement.ToVec3() * Time.deltaTime;
+        // Apply velocity
+        rb.velocity = targetVelocity;
 
-        CheckGround();
-
-        Vector3 contactPoint = groundRayOrigin.position + Vector3.down * groundRayLength;
-
-        if (lastGroundHit.transform != null && contactPoint.y < lastGroundHit.point.y) {
-            transform.position = new Vector3(transform.position.x, transform.position.y + (lastGroundHit.point.y - contactPoint.y), transform.position.z);
-        }
-    }
-
-    public void CheckGround () {
-        RaycastHit2D hit = Physics2D.Raycast(groundRayOrigin.position.ToVec2(), Vector2.down, groundRayLength, groundMask);
-
-        grounded = hit.transform != null;
-        lastGroundHit = hit;
-    }
-
-	
-    void OnDrawGizmos () {
-        Debug.DrawLine(groundRayOrigin.position, groundRayOrigin.position + Vector3.down * groundRayLength, Color.red);
+        // Check if grounded
+        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundRadius, groundMask) != null;
     }
 
 }
