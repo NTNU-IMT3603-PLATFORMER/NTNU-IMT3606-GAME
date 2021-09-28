83ec32f4d90cfbcfe6a0805a15a3781d189f023b
Ruben Christoffer <rubenc@protonmail.com>
2021-09-28 09:30:53 +0200

    Scripts: PlayerMovement: Added running

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public CharacterController2D cc;
+	public CharacterController2D characterController;
 	public float speed = 5f;
+	public float runSpeed = 8f;
 
-	public bool running { get; private set; }
-
-	private float move;
-	private bool jump;
+	public bool isMoving { get; private set; }
+	public bool isRunning { get; private set; }
 
 	void FixedUpdate () {
-		move = Input.GetAxisRaw("Horizontal") * speed;
-		jump = Input.GetButton("Jump");
-		running = move != 0f;
+		// Only change running state when grounded
+		// as it doesn't look right when you are able
+		// to change speed in air
+		if (characterController.isGrounded) {
+			isRunning = Input.GetButton("Run");
+		}
+
+		float move = Input.GetAxisRaw("Horizontal") * (isRunning ? runSpeed : speed);
+		bool jump = Input.GetButton("Jump");
+
+		isMoving = move != 0f;
         
-		cc.Move(Vector2.right * move, jump);
+		characterController.Move(Vector2.right * move, jump);
 	}
 
 }
