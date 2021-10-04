80f2fc3c740cea0ee05c50715a87344b11120082
Ruben Christoffer <rubenc@protonmail.com>
2021-10-04 23:59:56 +0200

    Scripts: Extensions: Added Clamp and WithinRange utility methods for Vector2

--- Assets/Scripts/Utils/Extensions.cs
+++ Assets/Scripts/Utils/Extensions.cs
@@ -12,4 +12,18 @@ public static class Extensions {
         return new Vector3(vector.x, vector.y, 0f);
     }
 
+    public static Vector2 Clamp (this Vector2 vector, Vector2 min, Vector2 max) {
+        vector.x = Mathf.Clamp(vector.x, min.x, max.x);
+        vector.y = Mathf.Clamp(vector.y, min.y, max.y);
+
+        return vector;
+    }
+
+    public static bool WithinRange (this Vector2 vector, Vector2 min, Vector2 max) {
+        return vector.x <= max.x 
+                && vector.x >= min.x 
+                && vector.y <= max.y 
+                && vector.y >= min.y;
+    }
+
 }
