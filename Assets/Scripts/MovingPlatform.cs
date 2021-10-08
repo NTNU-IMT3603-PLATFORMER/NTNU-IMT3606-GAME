d4ea61752c71e3493c8b5ba34108a26def86129f
Michael <32868357+Omega1a@users.noreply.github.com>
2021-10-08 13:41:04 +0200

    Fixed: Rigidbody into MovingPlatform script

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
-{
+{   
+    public Rigidbody2D rb;
     public List<Transform> waypoints;
     public float moveSpeed;
     public int target;
 
-    void Update()
+    /* void Update()
     {
         //receive data
         transform.position = Vector3.MoveTowards(transform.position, waypoints[target].position, moveSpeed * Time.deltaTime);
-    }
+    } */
 
     private void FixedUpdate() 
     {
+        rb.MovePosition(Vector3.MoveTowards(transform.position, waypoints[target].position, moveSpeed * Time.deltaTime));
         //use data
         if(transform.position == waypoints[target].position)
         {
