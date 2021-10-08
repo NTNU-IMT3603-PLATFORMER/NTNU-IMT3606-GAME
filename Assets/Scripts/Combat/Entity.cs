e1ba38d1c2201a0aff024251ee9113ad8badfcd4
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-10-08 13:06:24 +0200

    Added a _baseDamage field to Entity

--- Assets/Scripts/Combat/Entity.cs
+++ Assets/Scripts/Combat/Entity.cs
@@ -6,6 +6,8 @@ abstract class Entity : MonoBehaviour
 {
     [SerializeField, Tooltip("The health of the entity")]
     int _health;
+    [SerializeField, Tooltip("The damage the entity does to other entities")]
+    int _baseDamage;
 
     public abstract void TakeDamage(int damage);
     public abstract void Respawn();
