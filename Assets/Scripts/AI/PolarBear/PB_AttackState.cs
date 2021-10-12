117597732dbbf4797d3e5db2b0f483983da1436b
Ruben Christoffer <rubenc@protonmail.com>
2021-10-12 11:51:47 +0200

    Scripts: PB_AttackState: Now has basic attacking and charge capabilities

--- Assets/Scripts/AI/PolarBear/PB_AttackState.cs
+++ Assets/Scripts/AI/PolarBear/PB_AttackState.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class PB_AttackState : State<PB_Data> {
 
+    bool _charge;
+    bool _shockWave;
     Vector2 _move;
+    AttackStrategy _currentAttackStrategy;
+
+    enum AttackStrategy {
+        NormalAttack,
+        Charge,
+        Shockwave
+    }
+
+    // The time before the AI is allowed to change strategy
+    float _strategyDecisionCountdown;
 
     public override void OnEnterState() {
 
@@ -15,21 +27,53 @@ public class PB_AttackState : State<PB_Data> {
     }
 
     public override void OnUpdateState() {
-        // Are we close to the player?
+        _strategyDecisionCountdown -= Time.deltaTime;
+
+        if (_strategyDecisionCountdown <= 0f) {
+            UpdateStrategy();
+            _strategyDecisionCountdown = Random.Range(1f, 5f);
+
+            Debug.Log(_currentAttackStrategy);
+        }
+
+        // Are we within attacking distance to the player?
         if (data.player.transform.position.IsWithinDistanceOf(transform.position, 1f)) {
             _move = Vector2.zero;
+
+            if (_currentAttackStrategy == AttackStrategy.NormalAttack) {
+                // TODO: Perform normal attack here
+            }
         } else {
             Vector2 moveDirection = (data.player.transform.position - transform.position).normalized;
             moveDirection.y = 0f;
 
             _move = moveDirection * data.speed;
         }
+
+        // Are we within charging distance?
+        if (data.player.transform.position.IsWithinDistanceOf(transform.position, 8f) && _currentAttackStrategy == AttackStrategy.Charge) {
+            _charge = true;
+        } else {
+            _charge = false;
+        }
     }
 
     public override void OnFixedUpdateState() {
-        
+        data.characterController2D.Move(_move != Vector2.zero, _move, false, _charge);
 
-        data.characterController2D.Move(_move != Vector2.zero, _move, false, false);
+        if (_charge) {
+            UpdateStrategy();
+        }
+    }
+
+    void UpdateStrategy () {
+        if (Random.value < 0.33f) {
+            _currentAttackStrategy = AttackStrategy.Charge;
+        } else if (Random.value < 0.33f) {
+            _currentAttackStrategy = AttackStrategy.Shockwave;
+        } else {
+            _currentAttackStrategy = AttackStrategy.NormalAttack;
+        }
     }
 
 }
