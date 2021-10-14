767a43d01c200fd019ae6b1929faf2cde81bcdea
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-10-14 11:35:49 +0200

    Changed animation trigger to be activated with an Event

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -6,11 +6,19 @@ public class PlayerAnimation : MonoBehaviour {
 
     [SerializeField, Tooltip("Animator controlling animations")]        Animator _animator;
     [SerializeField, Tooltip("PlayerMovement connected to player")]     PlayerMovement _playerMovement;
+    [SerializeField, Tooltip("PlayerCombat connected to player")]     PlayerCombat _playerCombat;
 
+    private void Start() {
+        _playerCombat.eventOnAttack.AddListener(AttackAnimationListener);
+    }
     void Update () {
         _animator.SetBool("move", _playerMovement.isMoving && !_playerMovement.characterController.isDashing);
         _animator.SetBool("grounded", _playerMovement.characterController.isGrounded);
         _animator.SetBool("facingWall", _playerMovement.characterController.isHuggingWall);
+    }
+
+    void AttackAnimationListener() {
+        _animator.SetTrigger("isAttacking"); 
     }
 
 }
