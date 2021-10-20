fddc8d1afb040a37a7f3208e9859dcf5d6f5fb09
Ruben Christoffer <rubenc@protonmail.com>
2021-10-20 21:34:04 +0200

    Scripts: PB Boss is now invincible while charging

--- Assets/Scripts/Combat/Entity.cs
+++ Assets/Scripts/Combat/Entity.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public abstract class Entity : MonoBehaviour {
 
+<<<<<<< HEAD
     [SerializeField, Tooltip("The rigidbody of the entity")]
     Rigidbody2D _rigidbody;
 
@@ -12,22 +13,43 @@ public abstract class Entity : MonoBehaviour {
     int _health;
     [SerializeField, Tooltip("The damage the entity does to other entities")]
     int _baseDamage;
+=======
+    [SerializeField, Tooltip("The health of the entity")]                       int _health;
+    [SerializeField, Tooltip("The damage the entity does to other entities")]   int _baseDamage;
+    [SerializeField, Tooltip("If true, inflicting damage will have no effect")] bool _invincible;
+>>>>>>> c0d4f71 (Scripts: PB Boss is now invincible while charging)
 
 
     /// <summary>
     /// The damage the entity does to other entities
     /// </summary>
+<<<<<<< HEAD
     public int baseDamage {
         get => _baseDamage;
         set => _baseDamage = value;
     }
 
     public int health => _health;
+=======
+    public int baseDamage => _baseDamage;
+    
+    /// <summary>
+    /// If true, inflicting damage will have no effect
+    /// </summary>
+    public bool invincible {
+        get => _invincible;
+        set => _invincible = value;
+    }
+>>>>>>> c0d4f71 (Scripts: PB Boss is now invincible while charging)
 
     public abstract void Respawn();
     public abstract void Die();
 
-    public void InflictDamage(int damage) {
+    public virtual void InflictDamage(int damage) {
+        if (invincible) {
+            return;
+        }
+
         _health -= damage;
 
         if (_health <= 0) {
