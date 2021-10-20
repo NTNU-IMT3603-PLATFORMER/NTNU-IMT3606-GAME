a47201d80967b32aaaecb7b12885c77e72239133
Ruben Christoffer <rubenc@protonmail.com>
2021-10-20 14:32:19 +0200

    Effects: Added SpunUpDirt which will damage the player if it gets too close

---
+++ Assets/Scripts/SpunUpDirt.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpunUpDirt : MonoBehaviour {
+
+    [SerializeField, Tooltip("Damage inflicted on player on hit")]          int _moveSpeed = 5;
+    [SerializeField, Tooltip("Damage inflicted on player on hit")]          int _damage = 10;
+    [SerializeField, Tooltip("Cooldown for inflicting damage to player")]   float _damageCooldown = 0.5f;
+    [SerializeField, Tooltip("Radius for inflicting damage")]               float _damageRadius = 0.8f;
+
+    PlayerEntity _playerEntity;
+    float _timeLeftToInflictDamage;
+    bool _moveLeft;
+
+    /// <summary>
+    /// Should the dirt move left? (or right?)
+    /// </summary>
+    public bool moveLeft {
+        get => _moveLeft;
+        set => _moveLeft = value;
+    }
+
+    void Start () {
+        _playerEntity = GameObject.FindWithTag("Player").GetComponent<PlayerEntity>();
+    }
+
+    void Update () {
+        if (_timeLeftToInflictDamage <= 0f) {
+            if (_playerEntity.transform.position.IsWithinDistanceOf(transform.position, _damageRadius)){
+                _playerEntity.InflictDamage(_damage);
+                _timeLeftToInflictDamage = _damageCooldown;
+            }
+        } else {
+            _timeLeftToInflictDamage -= Time.deltaTime;
+        }
+
+        Vector3 direction = moveLeft ? Vector3.left : Vector3.right;
+        transform.Translate(direction * _moveSpeed * Time.deltaTime);
+    }
+
+}
