75206a28b7463407f939e19792d4c31cac591e7e
Ruben Christoffer <rubenc@protonmail.com>
2021-10-20 23:56:03 +0200

    Scripts: Added better way of staying on moving platform

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour
-{   
+public class MovingPlatform : MonoBehaviour {   
     public Rigidbody2D rb;
     public List<Transform> waypoints;
     public float moveSpeed;
     public int target;
 
-    private void FixedUpdate() 
-    {
-        rb.MovePosition(Vector3.MoveTowards(transform.position, waypoints[target].position, moveSpeed * Time.deltaTime));
+    float _distanceLeft;
+
+    private void FixedUpdate() {
+        if (_distanceLeft > 0f) {
+            _distanceLeft -= moveSpeed * Time.fixedDeltaTime;
+        }
+
+        Vector2 direction = (waypoints[target].position.ToVec2() - rb.position).normalized;
+        rb.velocity = moveSpeed * direction;
+
         //use data
-        if(rb.position.IsWithinDistanceOf(waypoints[target].position, 0.2f))
+        if(_distanceLeft <= 0f)
         {
             if(target == waypoints.Count - 1)
             {
@@ -23,6 +29,9 @@ public class MovingPlatform : MonoBehaviour
             {
                 target += 1;
             }
+
+            _distanceLeft = (waypoints[target].position.ToVec2() - rb.position).magnitude;
         }
     }
+
 }
