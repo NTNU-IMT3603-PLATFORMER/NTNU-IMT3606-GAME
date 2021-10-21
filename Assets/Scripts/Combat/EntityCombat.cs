a51866c4b76dd56323580e0175482fd2d482263c
Ruben Christoffer <rubenc@protonmail.com>
2021-10-21 13:00:12 +0200

    Fixed most of our merge conflict mess

--- Assets/Scripts/Combat/EntityCombat.cs
+++ Assets/Scripts/Combat/EntityCombat.cs
@@ -4,10 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class EntityCombat : MonoBehaviour {
+public abstract class EntityCombat : MonoBehaviour {
 
+    [SerializeField, Tooltip("The damage the entity does to other entities")]   int _baseDamage;
     [SerializeField, Tooltip("The range of the player attack as a float")]
     float _attackRange;
+    [SerializeField, Tooltip("How often the player can attack")]
+    float _attackRate;
 
     [SerializeField, Tooltip("The attack point of the player")]
     Transform attackPoint;
@@ -20,6 +23,30 @@ public class EntityCombat : MonoBehaviour {
     /// Unity event for when the player is attacking
     /// </summary>
     public UnityEvent eventOnAttack => _eventOnAttack;
+
+    /// <summary>
+    /// The damage the entity does to other entities
+    /// </summary>
+    public int baseDamage {
+        get => _baseDamage;
+        set => _baseDamage = value;
+    }
+
+    /// <summary>
+    /// The range of the player attack
+    /// </summary>
+    public float attackRange {
+        get => _attackRange;
+        set => _attackRange = value;
+    }
+
+    /// <summary>
+    /// How often the player can attack
+    /// </summary>
+    public float attackRate {
+        get => _attackRate;
+        set => _attackRate = value;
+    }
 
     /// <summary>
     /// Perform an attack.
@@ -45,6 +72,5 @@ public class EntityCombat : MonoBehaviour {
 
         Gizmos.DrawWireSphere(attackPoint.position, _attackRange);
     }
-
 
 }
--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour {
 
     [SerializeField, Tooltip("Animator controlling animations")]        Animator _animator;
-    [SerializeField, Tooltip("PlayerMovement connected to player")]     PlayerMovement _playerMovement;
-    [SerializeField, Tooltip("PlayerCombat connected to player")]     PlayerCombat _playerCombat;
-    [SerializeField, Tooltip("PlayerTransformation connected to player")] PlayerTransformation _playerTransformation;
 
-    private void Start() {
+    PlayerMovement _playerMovement;
+    PlayerCombat _playerCombat;
+    PlayerTransformation _playerTransformation;
+
+    void Start() {
+        _playerMovement = GetComponent<PlayerMovement>();
+        _playerCombat = GetComponent<PlayerCombat>();
+        _playerTransformation = GetComponent<PlayerTransformation>();
+
         _playerCombat.eventOnAttack.AddListener(AttackAnimationListener);
         _playerTransformation.eventOnTransform.AddListener(TransformAnimationListener);
-
     }
+
     void Update () {
         _animator.SetBool("move", _playerMovement.isMoving && !_playerMovement.characterController.isDashing);
         _animator.SetBool("grounded", _playerMovement.characterController.isGrounded);
@@ -27,4 +32,5 @@ public class PlayerAnimation : MonoBehaviour {
     void TransformAnimationListener() {
         _animator.SetTrigger("isTransforming");
     }
+
 }
