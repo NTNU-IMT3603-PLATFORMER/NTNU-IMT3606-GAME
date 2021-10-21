ea43ee2328c7b732eb9cedf41fcab1e923e1df69
Ruben Christoffer <rubenc@protonmail.com>
2021-10-21 13:28:07 +0200

    Scripts: Refactored combat system and entity system

--- Assets/Scripts/Combat/PlayerCombat.cs
+++ Assets/Scripts/Combat/PlayerCombat.cs
@@ -5,24 +5,12 @@ using UnityEngine.Events;
 
 public class PlayerCombat : EntityCombat {
 
-    Entity _player;
-    float _nextAttackTime;
+    public override void UpdateCombat() {
+        base.UpdateCombat();
 
-    void Start () {
-        _player = GetComponent<Entity>();
-    }
-
-    // Update is called once per frame
-    void Update() {
-        if (_nextAttackTime <= 0) {
-            if (Input.GetButtonDown("Attack")) {
-                Attack(baseDamage);
-                _nextAttackTime = attackRate;
-            }
-        } else {
-            _nextAttackTime -= Time.deltaTime; 
+        if (Input.GetButtonDown("Attack") && canAttack) {
+            Attack(baseDamage);
         }
     }
 
 }
-    
