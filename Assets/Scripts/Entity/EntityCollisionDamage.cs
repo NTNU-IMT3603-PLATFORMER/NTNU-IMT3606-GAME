c18265a008dd40686df112c7328f3a083bc6a4f8
Ruben Christoffer <rubenc@protonmail.com>
2021-10-27 13:21:54 +0200

    Scripts: Added EntityCollisionDamage

---
+++ Assets/Scripts/Entity/EntityCollisionDamage.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for dealing damage when colliding with other entities
+/// </summary>
+[RequireComponent(typeof(EntityCollision))]
+public class EntityCollisionDamage : MonoBehaviour {
+
+    [SerializeField, Tooltip("How much damage should the player take?")]                    int _damage = 1;
+
+    EntityCollision _entityCollision;
+    float _damageTimer;
+
+    void Start () {
+        _entityCollision = GetComponent<EntityCollision>();
+        
+        _entityCollision.eventOnEntityCollisionEnter.AddListener(OnEntityCollisionEnter);
+    }
+
+    void OnEntityCollisionEnter (Entity entity) {
+        entity.InflictDamage(_damage);
+    }
+
+}
