5d9ec1e956317b89718cdc14ad230d8e8d54a8cb
Ruben Christoffer <rubenc@protonmail.com>
2021-11-02 12:18:54 +0100

    Scripts: Improved thorn damage knockback fix

--- Assets/Scripts/Environment/Thorn.cs
+++ Assets/Scripts/Environment/Thorn.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Thorn : MonoBehaviour {
-    
+
     // Amount of damage the thorn should do
     const int THORN_DAMAGE = 1;
+
+    Collider2D _collider;
+
+    void Start () {
+        _collider = GetComponent<Collider2D>();
+    }
     
     // Inflicts damage if the game object is an entity
-    private void OnCollisionEnter2D(Collision2D collision) {
-        Entity entity = collision.transform.GetComponentInParent<Entity>();
+    private void OnTriggerEnter2D(Collider2D collider) {
+        Entity entity = collider.GetComponentInParent<Entity>();
+
         if (entity != null){
-            entity.InflictDamage(THORN_DAMAGE, collision.GetContact(0).point, 10f);
+            entity.InflictDamage(THORN_DAMAGE, _collider.ClosestPoint(entity.transform.position), 10f);
         }
     }
 
