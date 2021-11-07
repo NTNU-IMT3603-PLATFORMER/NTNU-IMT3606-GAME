36328198a076d6d579024c1809499e4d92ff1667
Ruben Christoffer <rubenc@protonmail.com>
2021-11-07 18:34:08 +0100

    Scripts: VolumeOption: Volume is now saved and loaded when starting game #65

--- Assets/Scripts/UI/VolumeOption.cs
+++ Assets/Scripts/UI/VolumeOption.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class VolumeOption : MonoBehaviour {
+public class VolumeOption : MonoBehaviour, IOption {
 
-    [SerializeField, Tooltip("Text displaying volume percentage")]   TMPro.TextMeshProUGUI _volumePercentageText;
+    [SerializeField, Tooltip("Text displaying volume percentage")]      TMPro.TextMeshProUGUI _volumePercentageText;
+    [SerializeField, Tooltip("Slider for volume percentage")]           Slider _volumeSlider;
+
+    public void InitializeOption () {
+        // Load previous volume settings
+        float volumePercentage = PlayerPrefs.GetFloat("Volume", 1f);
+
+        UpdateText(volumePercentage);
+        _volumeSlider.value = volumePercentage;
+        AudioListener.volume = volumePercentage;
+    }
 
     public void OnVolumeChanged (float volumePercentage) {
-        // Set volume text with appropriate amount of decimals
-        _volumePercentageText.text = (volumePercentage * 100f).ToString("0.0") + "%";
+        UpdateText(volumePercentage);
 
         // Update global volume
         AudioListener.volume = volumePercentage;
+
+        // Save volume settings
+        PlayerPrefs.SetFloat("Volume", volumePercentage);
+        PlayerPrefs.Save();
+    }
+
+    void UpdateText (float volumePercentage) {
+        // Set volume text with appropriate amount of decimals
+        _volumePercentageText.text = (volumePercentage * 100f).ToString("0.0") + "%";
     }
 
 }
