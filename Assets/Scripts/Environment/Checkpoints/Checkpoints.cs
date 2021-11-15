833e6f0ac02e5558beb8e43f65cf3e8b46092c34
Ruben Christoffer <rubenc@protonmail.com>
2021-11-15 18:51:28 +0100

    Scripts: PlayerEntity: Respawn point is now moved when passing checkpoint #78

--- Assets/Scripts/Environment/Checkpoints/Checkpoints.cs
+++ Assets/Scripts/Environment/Checkpoints/Checkpoints.cs
@@ -11,14 +11,19 @@ using UnityEngine.Events;
 /// </summary>
 public class Checkpoints : MonoBehaviour {
 
+    /// <summary>
+    /// Global instance of Checkpoints
+    /// </summary>
+    public static Checkpoints INSTANCE { get; private set; }
+
     Checkpoint[] _checkpoints;
     int _currentCheckpoint = -1;
-    UnityEvent<int> _eventOnReachedCheckpoint = new UnityEvent<int>();
+    UnityEvent<Checkpoint> _eventOnReachedCheckpoint = new UnityEvent<Checkpoint>();
 
     /// <summary>
     /// Event for when player has reached a new checkpoint
     /// </summary>
-    public UnityEvent<int> eventOnReachedCheckpoint => _eventOnReachedCheckpoint;
+    public UnityEvent<Checkpoint> eventOnReachedCheckpoint => _eventOnReachedCheckpoint;
 
     /// <summary>
     /// The checkpoint the player has reached and can respawn at.
@@ -31,11 +36,12 @@ public class Checkpoints : MonoBehaviour {
 
         if (currentCheckpoint < index) {
             _currentCheckpoint = index;
-            _eventOnReachedCheckpoint.Invoke(index);
+            _eventOnReachedCheckpoint.Invoke(checkpoint);
         }
     }
 
-    void Start () {
+    void Awake () {
+        INSTANCE = this;
         _checkpoints = GetComponentsInChildren<Checkpoint>();
     }
 
