2a511006ee522e158da6893f898a19ac7ed44884
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-16 14:44:49 +0100

    Basic trigger dialogue function

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Dialogue 
-{
-        [SerializeField]    string _name;
-        [SerializeField]    string[] _sentences;
+public class Dialogue {
+    [SerializeField]    string _name;
+    [TextArea(3, 10)]
+    [SerializeField]    string[] _sentences;
+
+
+    public string name => _name;
+
+    public string[] sentences => _sentences;
+
        
 }
 
