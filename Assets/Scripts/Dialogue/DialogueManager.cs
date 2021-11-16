d470eee0b15e6fb21007f68620ca4c1b765725c3
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-16 14:20:17 +0100

    Added empty dialogue scripts

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    Queue<string> _sentences;
+
+    // Start is called before the first frame update
+    void Start() {
+        _sentences = new Queue<string>();
+    }
+
+}
