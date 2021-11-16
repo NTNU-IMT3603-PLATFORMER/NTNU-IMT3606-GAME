499db4f2bdc75914008e5c12502d80fa626e08b5
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-16 15:36:34 +0100

    Dialogue kinda works :)

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour 
 {
     public Dialogue dialogue;
-
     Vector3 _npcPosition;
-
+    bool _firstTalk;
 
     private void Start() {
         _npcPosition = transform.position;
+        _firstTalk = true;
     }
 
     public void TriggerDialogue ()
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        if(_firstTalk) {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            _firstTalk = false;
+        } else {
+            _firstTalk = FindObjectOfType<DialogueManager>().DisplayNextSentence();
+        }
     }
 
     void Update() {
         if(Vector3.Distance(PlayerEntity.INSTANCE.transform.position, _npcPosition) < 3) {
             //TODO: add something which indicates the npc is interactable
-            if(Input.GetButtonDown("Talk")) {
+            if(Input.GetButtonUp("Talk")) {
                 TriggerDialogue();
             }
         }
