8ee2458f8717b2c2721c77550215f7cb1e7624be
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-17 13:42:54 +0100

    Added Ui for dialogue

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -7,28 +7,34 @@ public class DialogueTrigger : MonoBehaviour
     public Dialogue dialogue;
     Vector3 _npcPosition;
     bool _firstTalk;
+    DialogueManager _dialogueManager;
 
     private void Start() {
         _npcPosition = transform.position;
         _firstTalk = true;
+        _dialogueManager = FindObjectOfType<DialogueManager>();
     }
 
     public void TriggerDialogue ()
     {
         if(_firstTalk) {
-            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            _dialogueManager.StartDialogue(dialogue);
             _firstTalk = false;
         } else {
-            _firstTalk = FindObjectOfType<DialogueManager>().DisplayNextSentence();
+            _firstTalk = _dialogueManager.DisplayNextSentence();
         }
     }
 
     void Update() {
-        if(Vector3.Distance(PlayerEntity.INSTANCE.transform.position, _npcPosition) < 3) {
-            //TODO: add something which indicates the npc is interactable
-            if(Input.GetButtonUp("Talk")) {
+        if (Input.GetButtonUp("Talk")) {
+            if (Vector3.Distance(PlayerEntity.INSTANCE.transform.position, _npcPosition) < 2) {
+                //TODO: add something which indicates the npc is interactable
                 TriggerDialogue();
-            }
+            } 
         }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        _dialogueManager.ResetDialogue(dialogue);     
     }
 }
