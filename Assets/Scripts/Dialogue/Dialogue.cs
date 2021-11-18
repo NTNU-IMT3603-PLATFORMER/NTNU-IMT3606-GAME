dac8e3b6e433bd1fdd178247e3e64d73b11b0fbf
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-18 10:42:11 +0100

    Added comments

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class Dialogue {
     [SerializeField]                    string _name;
     [SerializeField, TextArea(3, 10)]   string[] _sentences;
-
-
+    
+    /// <summary>
+    /// The name of the npc talking
+    /// </summary>
     public string name => _name;
 
+    /// <summary>
+    /// An array of sentences the npc is saying
+    /// </summary>
     public string[] sentences => _sentences;
-
-       
 }
 
