d83f0dd27f414142208efaa574211acd786d9b0b
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-18 10:42:11 +0100

    Added comments

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -19,6 +19,10 @@ public class DialogueManager : MonoBehaviour {
         _textBoxCanvas.enabled = false;
     }
 
+    /// <summary>
+    /// Enqueues sentence in a queue and shows the first sentence
+    /// </summary>
+    /// <param name="dialogue">A dialogue object which contains a name and sentences</param>
     public void StartDialogue(Dialogue dialogue) {
         _nameText.text = dialogue.name;
         _sentences.Clear();
@@ -31,6 +35,10 @@ public class DialogueManager : MonoBehaviour {
         _textBoxCanvas.enabled = true;
     }
 
+    /// <summary>
+    /// Displays the next sentence in the queue
+    /// </summary>
+    /// <returns>Returns true if no more sentences, false otherwise</returns>
     public bool DisplayNextSentence() {
         if(_sentences.Count == 0) {
             EndDialogue();
@@ -42,8 +50,10 @@ public class DialogueManager : MonoBehaviour {
         return false;
     }
 
+    /// <summary>
+    /// Clears the sentence queue and disables dialogue canvas
+    /// </summary>
     public void EndDialogue() {
-        Debug.Log("End of conversation");
         _sentences.Clear();
         _textBoxCanvas.enabled = false;
     }
--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -2,20 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(EntityCollision))]
 public class DialogueTrigger : MonoBehaviour 
 {
     [SerializeField]    Dialogue _dialogue;
 
     DialogueManager _dialogueManager;
+    EntityCollision _entityCollision;
     Vector3 _npcPosition;
     bool _firstTalk;
 
-    private void Start() {
+    void Start() {
         _npcPosition = transform.position;
         _firstTalk = true;
         _dialogueManager = FindObjectOfType<DialogueManager>();
+        _entityCollision = GetComponent<EntityCollision>();
+        _entityCollision.eventOnEntityCollisionExit.AddListener(OnEntityTriggerExit);
     }
 
+    void Update() {
+        if (Input.GetButtonDown("Talk")) {
+            if (Vector3.Distance(PlayerEntity.INSTANCE.transform.position, _npcPosition) < 2) {
+                //TODO: add something which indicates the npc is interactable
+                TriggerDialogue();
+            } 
+        }
+    }
+
+    void OnEntityTriggerExit(Entity entity) {
+        _dialogueManager.EndDialogue();
+        _firstTalk = true;
+    }
+
+    /// <summary>
+    /// Ran when dialogue is triggered,
+    /// checks if you are already in a conversation with the npc
+    /// </summary>
     public void TriggerDialogue ()
     {
         if(_firstTalk) {
@@ -26,17 +48,4 @@ public class DialogueTrigger : MonoBehaviour
         }
     }
 
-    void Update() {
-        if (Input.GetButtonUp("Talk")) {
-            if (Vector3.Distance(PlayerEntity.INSTANCE.transform.position, _npcPosition) < 2) {
-                //TODO: add something which indicates the npc is interactable
-                TriggerDialogue();
-            } 
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other) {
-        _dialogueManager.EndDialogue();
-        _firstTalk = true;
-    }
 }
