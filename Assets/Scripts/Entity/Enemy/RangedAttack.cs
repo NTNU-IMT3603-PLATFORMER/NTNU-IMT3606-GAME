bf80db4b8cac517f1a89557443ba165cb18632f6
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-24 16:41:51 +0100

    Projectile now does damage

--- Assets/Scripts/Entity/Enemy/RangedAttack.cs
+++ Assets/Scripts/Entity/Enemy/RangedAttack.cs
@@ -7,24 +7,30 @@ public class RangedAttack : MonoBehaviour {
     Transform _player;
     Vector3 _movementDirection;
     float _timeLeft = 2f;
-    
-   // Start is called before the first frame update
+
+    // Start is called before the first frame update
     void Start() {
         _player = PlayerEntity.INSTANCE.transform;
-        if(transform.position.x > _player.position.x) {
-            _movementDirection = new Vector3(-1, 0, 0); 
+        if (transform.position.x > _player.position.x) {
+            _movementDirection = new Vector3(-1, 0, 0);
         } else {
-            _movementDirection = new Vector3(1, 0, 0); 
+            _movementDirection = new Vector3(1, 0, 0);
         }
     }
 
     // Update is called once per frame
     void Update() {
-        if(_timeLeft > 0) {
+        if (_timeLeft > 0) {
             transform.Translate(_movementDirection * Time.deltaTime * 5);
             _timeLeft -= Time.deltaTime;
         } else {
             Destroy(gameObject);
-        } 
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            collision.GetComponent<Entity>().InflictDamage(1, transform.position, 5);
+        }
     }
 }
