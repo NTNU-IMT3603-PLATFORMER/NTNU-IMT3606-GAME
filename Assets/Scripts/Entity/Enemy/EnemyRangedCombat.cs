ef4c6f409bd34608c9bdbcc9d4fd82f3fd29d1b8
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-24 16:34:09 +0100

    Enemy fires gameObject towards Player

--- Assets/Scripts/Entity/Enemy/EnemyRangedCombat.cs
+++ Assets/Scripts/Entity/Enemy/EnemyRangedCombat.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class EnemyRangedCombat : EnemyCombat {
 
+    [SerializeField]    Transform _prefabRangedAttack;
 
+    /// <summary>
+    /// Ranged attack
+    /// </summary>
+    /// <param name="damage">The damage the attack does</param>
     public override void Attack(int damage) {
-
+        // Instantiate a ranged attack at the attack position
+        Instantiate(_prefabRangedAttack, attackPoint.position, Quaternion.identity);
         
 
         timeLeftToAllowAttack = attackCooldown;
