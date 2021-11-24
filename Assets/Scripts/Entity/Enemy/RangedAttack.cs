5b501b545948728d3f2b12fd888343bf73a2f2d1
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-24 16:34:09 +0100

    Enemy fires gameObject towards Player

--- Assets/Scripts/Entity/Enemy/RangedAttack.cs
+++ Assets/Scripts/Entity/Enemy/RangedAttack.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RangedAttack : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
 
+    Transform _player;
+    Vector3 _movementDirection;
+    float _timeLeft = 2f;
+    
+   // Start is called before the first frame update
+    void Start() {
+        _player = PlayerEntity.INSTANCE.transform;
+        if(transform.position.x > _player.position.x) {
+            _movementDirection = new Vector3(-1, 0, 0); 
+        } else {
+            _movementDirection = new Vector3(1, 0, 0); 
+        }
     }
 
     // Update is called once per frame
     void Update() {
-
+        if(_timeLeft > 0) {
+            transform.Translate(_movementDirection * Time.deltaTime * 5);
+            _timeLeft -= Time.deltaTime;
+        } else {
+            Destroy(gameObject);
+        } 
     }
 }
