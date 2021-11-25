d660afdb77c10a6854ed2784e4838410a4563409
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-25 12:57:59 +0100

    Fixes

--- Assets/Scripts/Entity/Enemy/RangedAttack.cs
+++ Assets/Scripts/Entity/Enemy/RangedAttack.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class RangedAttack : MonoBehaviour {
 
     Transform _player;
-    Vector3 _movementDirection;
+    Rigidbody2D _rigidbody;
     float _timeLeft = 2f;
+
 
     // Start is called before the first frame update
     void Start() {
         _player = PlayerEntity.INSTANCE.transform;
-        _movementDirection = (_player.position - transform.position).normalized;
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = (_player.position - transform.position).normalized * 5;
     }
 
     // Update is called once per frame
     void Update() {
         if (_timeLeft > 0) {
-            transform.Translate(_movementDirection * Time.deltaTime * 5);
             _timeLeft -= Time.deltaTime;
         } else {
             Destroy(gameObject);
@@ -27,6 +28,8 @@ public class RangedAttack : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player") {
             collision.GetComponent<Entity>().InflictDamage(1, transform.position, 5);
+        } else {
+            Destroy(gameObject);
         }
     }
 }
