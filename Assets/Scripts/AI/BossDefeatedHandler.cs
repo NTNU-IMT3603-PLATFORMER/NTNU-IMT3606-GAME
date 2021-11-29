c87a697c85e293028e362c46cec12f6d4f3741be
Ruben Christoffer <rubenc@protonmail.com>
2021-11-29 15:50:06 +0100

    Added support for screen fading and loading next level on defeating boss #106

---
+++ Assets/Scripts/AI/BossDefeatedHandler.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Handles what happens when you defeat a boss
+/// </summary>
+public class BossDefeatedHandler : MonoBehaviour {
+
+    const float TIME_TO_LOAD_NEXT_LEVEL = 5f;
+
+    [SerializeField] EnemyEntity _boss;
+    [SerializeField] string _nextLevelName;
+
+    UnityEvent _eventOnDefeatedBoss = new UnityEvent();
+    bool _defeatedBoss;
+
+    /// <summary>
+    /// Returns the instance of BossDefeatedHandler
+    /// There should only be 1 BossDefeatedHandler at any given time
+    /// </summary>
+    public static BossDefeatedHandler INSTANCE { get; private set; }
+
+    /// <summary>
+    /// Event triggered by defeating boss on current level
+    /// </summary>
+    public UnityEvent eventOnDefeatedBoss => _eventOnDefeatedBoss;
+
+    /// <summary>
+    /// Have we defeated the boss on the current level?
+    /// </summary>
+    public bool defeatedBoss => _defeatedBoss;
+
+    void Awake () {
+        INSTANCE = this;
+
+        _boss.eventOnDeath.AddListener(() => StartCoroutine(OnDeath()));
+    }
+
+    IEnumerator OnDeath () {
+        _defeatedBoss = true;
+        _eventOnDefeatedBoss.Invoke();
+        yield return new WaitForSeconds(TIME_TO_LOAD_NEXT_LEVEL);
+        SceneManager.LoadScene(_nextLevelName);
+    }
+
+}
---
+++ Assets/Scripts/UI/FadeOnDefeatedBoss.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Activates fading when boss is defeated
+/// </summary>
+[RequireComponent(typeof(FadePanel))]
+public class FadeOnDefeatedBoss : MonoBehaviour {
+
+    FadePanel _fadePanel;
+
+    void Start () {
+        _fadePanel = GetComponent<FadePanel>();
+
+        if (BossDefeatedHandler.INSTANCE != null) {
+            BossDefeatedHandler.INSTANCE.eventOnDefeatedBoss.AddListener(() => _fadePanel.fadeTarget = 1f);
+        }
+    }
+
+}
