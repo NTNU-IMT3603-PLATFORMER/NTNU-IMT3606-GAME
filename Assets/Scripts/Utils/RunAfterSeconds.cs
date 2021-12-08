1ec9b65632548dc03c9758f7668e38cdcb21f1e1
Mikkel Aas <aa.mikkel2902@gmail.com>
2021-12-08 11:28:37 +0100

    Cleaned up the Utils folder.

--- Assets/Scripts/Utils/RunAfterSeconds.cs
+++ Assets/Scripts/Utils/RunAfterSeconds.cs
@@ -11,29 +11,36 @@ using UnityEngine;
 /// </summary>
 public class RunAfterSeconds : MonoBehaviour {
 
+    /// <summary>
+    /// secondsToWait is how many seconds before you can run.
+    /// </summary>
     public float secondsToWait { get; private set; }
+
+    /// <summary>
+    /// action is the action that will be invoked after the timer.
+    /// </summary>
     public System.Action action { get; private set; }
-
-    void Start () {
-        StartCoroutine(RunWhenReady());
-    }
-
-    IEnumerator RunWhenReady () {
-        yield return new WaitForSeconds(secondsToWait);
-        action();
-        Destroy(gameObject);
-    }
 
     /// <summary>
     /// Create a gameobject that will carry out given action
     /// after a certain amount of seconds
     /// </summary>
-    public static void Create (float secondsToWait, System.Action action) {
+    public static void Create(float secondsToWait, System.Action action) {
         GameObject obj = new GameObject("RunAfterSecondsJob");
         RunAfterSeconds script = obj.AddComponent<RunAfterSeconds>();
 
         script.secondsToWait = secondsToWait;
         script.action = action;
+    }
+
+    IEnumerator RunWhenReady() {
+        yield return new WaitForSeconds(secondsToWait);
+        action();
+        Destroy(gameObject);
+    }
+
+    void Start() {
+        StartCoroutine(RunWhenReady());
     }
 
 }
