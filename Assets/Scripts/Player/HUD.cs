e122c4cc86241fdd6129827c9a19031b45efb3b3
Mikkel Aas <aa.mikkel2902@gmail.com>
2021-12-08 11:09:38 +0100

    Cleaned up the Player folder.

--- Assets/Scripts/Player/HUD.cs
+++ Assets/Scripts/Player/HUD.cs
@@ -4,21 +4,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HUD : MonoBehaviour{
-
+/// <summary>
+/// HUD renders the health and the blood bar in the game HUD.
+/// </summary>
+public class HUD : MonoBehaviour {
 
     [Header("Hearts")]
-    [SerializeField]        GameObject _heartsHolder;
-    [SerializeField]        Sprite _heart_full;
-    [SerializeField]        Sprite _heart_empty;
+    [SerializeField]
+    GameObject _heartsHolder;
+    [SerializeField]
+    Sprite _heart_full;
+    [SerializeField]
+    Sprite _heart_empty;
 
     [Header("Blood Bar")]
-    [SerializeField]        Slider _slider;
+    [SerializeField]
+    Slider _slider;
 
     Image[] _hearts;
     Image _fill;
 
-    void Start () {
+    void Start() {
         _hearts = _heartsHolder.GetComponentsInChildren<Image>();
         _fill = _slider.GetComponentInChildren<Image>();
     }
@@ -28,21 +34,29 @@ public class HUD : MonoBehaviour{
         UpdateBloodBar();
     }
 
+    /// <summary>
+    /// UpdateHealthBar fills the heart bar in the HUD for each heart the player has.
+    /// </summary>
     public void UpdateHealthBar() {
         for (int i = 0; i < _hearts.Length; i++) {
-            if(i <= PlayerEntity.INSTANCE.health) {
+            if (i <= PlayerEntity.INSTANCE.health) {
                 _hearts[i].sprite = _heart_full;
             } else {
                 _hearts[i].sprite = _heart_empty;
             }
         }
     }
+
+    /// <summary>
+    /// UpdateBloodBar fills the blood bar in the HUD.
+    /// If the of the bar is less than 3, it is white, else it is red.
+    /// </summary>
     public void UpdateBloodBar() {
         _slider.value = PlayerEntity.INSTANCE.bloodlevel;
         if (_slider.value < 3) {
             _fill.color = Color.white;
         } else {
-            _fill.color = Color.red; 
+            _fill.color = Color.red;
         }
     }
 
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	[SerializeField, Tooltip("Controller that will move character")]                                    CharacterController2D _characterController;
-	[SerializeField, Tooltip("Normal walking speed")]                                                   float _speed = 5f;
-	[SerializeField, Tooltip("Speed when running")]                                                     float _runSpeed = 8f;
+    [SerializeField, Tooltip("Controller that will move character")]
+    CharacterController2D _characterController;
+    [SerializeField, Tooltip("Normal walking speed")]
+    float _speed = 5f;
+    [SerializeField, Tooltip("Speed when running")]
+    float _runSpeed = 8f;
 
-	/// <summary>
-	/// Character controller used for player movement
-	/// </summary>
-	public CharacterController2D characterController => _characterController;
+    /// <summary>
+    /// Character controller used for player movement
+    /// </summary>
+    public CharacterController2D characterController => _characterController;
+
+    /// <summary>
+    /// runSpeed is the speed of the player when he/she is running (Holding SHIFT). 
+    /// </summary>
     public float runSpeed => _runSpeed;
 
+    /// <summary>
+    /// speed is the speed of the player when he/she is not running.
+    /// </summary>
     public float speed {
         get => _speed;
         set => _speed = value;
@@ -22,57 +32,57 @@ public class PlayerMovement : MonoBehaviour {
     /// <summary>
     /// Is the player moving?
     /// </summary>
-    public bool isMoving { get; private set; }
+    public bool isMoving { get; private set; }
 
-	/// <summary>
-	/// Should the player run once it starts moving?
-	/// Does not necessarily mean that the player is moving,
-	/// only that the player should run if it begins to move
-	/// </summary>
-	public bool shouldRun { get; private set; }
+    /// <summary>
+    /// Should the player run once it starts moving?
+    /// Does not necessarily mean that the player is moving,
+    /// only that the player should run if it begins to move
+    /// </summary>
+    public bool shouldRun { get; private set; }
 
-	float _move;
-	bool _jump;
-	bool _dash;
+    float _move;
+    bool _jump;
+    bool _dash;
 
-	void Update () {
-		// Only change running state when grounded
-		// as it doesn't look right when you are able
-		// to change speed in air
-		if (characterController.isGrounded) {
-			shouldRun = Input.GetButton("Run");
-		}
+    void Update() {
+        // Only change running state when grounded
+        // as it doesn't look right when you are able
+        // to change speed in air
+        if (characterController.isGrounded) {
+            shouldRun = Input.GetButton("Run");
+        }
 
-		_move = Input.GetAxisRaw("Horizontal") * (shouldRun ? _runSpeed : _speed);
+        _move = Input.GetAxisRaw("Horizontal") * (shouldRun ? _runSpeed : _speed);
 
-		if (Input.GetButton("Jump")) {
-			if (characterController.currentJumps == 0) {
-				_jump = true;
-			} else if (Input.GetButtonDown("Jump")) {
-				_jump = true;
-			}
-		}
+        if (Input.GetButton("Jump")) {
+            if (characterController.currentJumps == 0) {
+                _jump = true;
+            } else if (Input.GetButtonDown("Jump")) {
+                _jump = true;
+            }
+        }
 
-		if (Input.GetButtonDown("Dash")) {
-			_dash = true;
-		}
+        if (Input.GetButtonDown("Dash")) {
+            _dash = true;
+        }
 
-		// If the player is running and is on the ground, then play the running sound
-		if (isMoving && characterController.isGrounded){
-			AudioManager.instance.PlaySound("playerrun");
-		} else {
-			AudioManager.instance.StopSound("playerrun");
-		}
+        // If the player is running and is on the ground, then play the running sound
+        if (isMoving && characterController.isGrounded) {
+            AudioManager.instance.PlaySound("playerrun");
+        } else {
+            AudioManager.instance.StopSound("playerrun");
+        }
 
-		isMoving = _move != 0f;
-	}
+        isMoving = _move != 0f;
+    }
 
-	void FixedUpdate () {
-		_characterController.Move(Input.GetAxisRaw("Horizontal") != 0f, Vector2.right * _move, _jump, _dash);
-		
-		// Reset jump & dash so that input has to be captured again
-		_jump = false;
-		_dash = false;
-	}
+    void FixedUpdate() {
+        _characterController.Move(Input.GetAxisRaw("Horizontal") != 0f, Vector2.right * _move, _jump, _dash);
+
+        // Reset jump & dash so that input has to be captured again
+        _jump = false;
+        _dash = false;
+    }
 
 }
