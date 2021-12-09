75d8399c955b62d1c8979ce862009b025b135681
Erlend <erlend.johan.vannebo@gmail.com>
2021-12-09 16:41:10 +0100

    Code cleanup for UI, Spirits, moving platforms and spun up dirt #143

---
+++ Assets/Scripts/Environment/SpunUpDirt.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the attacking dirt from the shockwave attack the boss performs. 
+/// </summary>
+public class SpunUpDirt : MonoBehaviour {
+
+    [SerializeField, Tooltip("Damage inflicted on player on hit")]
+    int _moveSpeed = 5;
+    [SerializeField, Tooltip("Damage inflicted on player on hit")]
+    int _damage = 10;
+    [SerializeField, Tooltip("Cooldown for inflicting damage to player")]
+    float _damageCooldown = 0.5f;
+    [SerializeField, Tooltip("Radius for inflicting damage")]
+    float _damageRadius = 0.8f;
+
+    float _timeLeftToInflictDamage;
+    bool _moveLeft;
+
+    /// <summary>
+    /// Should the dirt move left? (or right?)
+    /// </summary>
+    public bool moveLeft {
+        get => _moveLeft;
+        set => _moveLeft = value;
+    }
+
+    PlayerEntity _playerEntity => PlayerEntity.INSTANCE;
+
+    void Update () {
+        if (_timeLeftToInflictDamage <= 0f) {
+            if (_playerEntity.transform.position.IsWithinDistanceOf(transform.position, _damageRadius)){
+                _playerEntity.InflictDamage(_damage, transform.position, 10f);
+                _timeLeftToInflictDamage = _damageCooldown;
+            }
+        } else {
+            _timeLeftToInflictDamage -= Time.deltaTime;
+        }
+
+        Vector3 direction = moveLeft ? Vector3.left : Vector3.right;
+        transform.Translate(direction * _moveSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/LevelMenu/OpenLevelMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLevelMenu : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject levelMenu;
+
+    bool _isAlreadyOpen;
+
+    
+    void Update() {
+        float distanceToPlayer = Vector3.Distance(PlayerEntity.INSTANCE.transform.position, transform.position);
+        if (Input.GetButtonDown("Talk")) {
+            if (distanceToPlayer < 2 && _isAlreadyOpen == false) {
+                //TODO: add something which indicates the npc is interactable
+                openLevelMenu(false);
+            } else if (distanceToPlayer < 2) {
+                openLevelMenu(true);
+            } 
+        }
+
+        
+
+        if (distanceToPlayer < 5 && distanceToPlayer > 2) {
+            //TODO: add something which indicates the npc is interactable
+            openLevelMenu(true);
+        }
+    }
+
+    public void openLevelMenu(bool isOpen) {
+        if(isOpen) {
+            Debug.Log(":)");
+            levelMenu.SetActive(false);
+            _isAlreadyOpen = false;
+        } else {
+            Debug.Log("no bør du funk for faen");
+            levelMenu.SetActive(true);
+            GameObject.FindObjectOfType<IncreaseValues>(true).UpdateValues();
+            _isAlreadyOpen = true;
+        }
+        
+    }
+}
