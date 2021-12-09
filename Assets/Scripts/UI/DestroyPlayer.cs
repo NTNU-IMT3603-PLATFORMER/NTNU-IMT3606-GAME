66367b710888eebabf267739169823d158ece2df
Ruben Christoffer <rubenc@protonmail.com>
2021-12-09 14:30:45 +0100

    Fixed player object causing problems when starting new game after finishing game #148

---
+++ Assets/Scripts/UI/DestroyPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains a simple function
+/// for destroying player object
+/// Used when exiting from credits
+/// to main menu
+/// </summary>
+public class DestroyPlayer : MonoBehaviour {
+
+    /// <summary>
+    /// Destroy player game object
+    /// </summary>
+    public void DestroyPlayerObject () {
+        Destroy(PlayerEntity.INSTANCE.gameObject);
+    }
+
+}
